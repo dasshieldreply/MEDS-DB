84b6a1785afe4ac6bda8c457f7a580f2122c4fae
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-04-15 20:28:48 +0100

    Columns added to STG_FILE_ROW

--- scripts/sequences_utils.sql
+++ scripts/sequences_utils.sql
@@ -9,7 +9,7 @@ alter sequence MEIC_NUMBER_SEQUENCE restart start with 7294;
 alter table job_tracking modify meic_number default MEIC_NUMBER_SEQUENCE.nextval;
 
 
-create sequence job_number_sequence  minvalue 1 maxvalue 999999999999999999999999999 increment by 1 start with 102533 nocache  noorder  nocycle  nokeep  noscale  global ;
+create sequence job_number_sequence  minvalue 1 maxvalue 999999999999999999999999999 increment by 1 start with 102534 nocache  noorder  nocycle  nokeep  noscale  global ;
 
 alter table meds_processing_job modify job_number default job_number_sequence.nextval;
 
