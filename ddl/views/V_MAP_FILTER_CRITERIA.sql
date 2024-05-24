fcbd479143b6f474c22cfa6648236180fe548c9c
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-05-24 17:37:28 +0100

    V_MAP_FILTER_CRITERIA v1 saved

--- ddl/views/V_MAP_FILTER_CRITERIA.sql
+++ ddl/views/V_MAP_FILTER_CRITERIA.sql
@@ -32,6 +32,8 @@ with fltr as
    ,  b.label layer_label
    ,  b.icon
    ,  b.color
+   ,  b.usage
+   ,  b.job_type_number
    from   medsfilter a
    ,      medslayer  b
    where  ':' || a.layerstring || ':' like '%:' || b.label || ':%'
@@ -41,23 +43,28 @@ select
 ,  a.filter_label
 ,  a.medslayer
 ,  a.layer_label
-,  b.job_number
-,  b.meds_cruise_number 
+,  c.job_number
+,  c.meds_cruise_number 
 ,  a.date_start
 ,  nvl(a.date_end,sysdate)
-,  b.meic_number
-,  b.originator 
-,  c.supplier
+,  c.meic_number
+,  c.originator 
+,  d.supplier
 ,  'fa ' || nvl(a.icon, 'fa-map_marker')
 ,  a.color
 ,  a.location_rectangle
 from   fltr                  a
-,      meds_processing_job   b
-,      job_tracking          c
-where  (a.meic_number        is null or b.meic_number          = a.meic_number)
-and    (a.meds_job_number    is null or b.job_number           = a.meds_job_number)
-and    (a.meds_cruise_number is null or b.meds_cruise_number   = a.meds_cruise_number)
-and    (a.originator         is null or b.originator           = a.originator)
-and     c.meic_number                                          = b.meic_number
-and    (a.supplier           is null or c.supplier             = a.supplier)
+,      job_lookups           b
+,      meds_processing_job   c
+,      job_tracking          d
+where  b.type = 'Data Type'
+and    b.usage = a.usage
+and   (c.data_type        = b.usage or c.data_type          = b.description)               -- will select jobs that have the data type of the layer. The data_type field can have usage or description as value
+and   (a.meic_number        is null or c.meic_number        = a.meic_number)
+and   (a.meds_job_number    is null or c.job_number         = a.meds_job_number)
+and   (a.meds_cruise_number is null or c.meds_cruise_number = a.meds_cruise_number)
+and   (a.originator         is null or c.originator         = a.originator)
+and    d.meic_number      = c.meic_number
+and    d.job_type_number  = a.job_type_number   -- for the different SERD tyypes the data types is the same, but the job type is not
+and   (a.supplier           is null or d.supplier           = a.supplier)  
 ;
