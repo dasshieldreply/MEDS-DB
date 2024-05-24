0c75a4643c501c4707379d658af59321530d32a3
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-05-24 10:49:37 +0100

    User_Questions updated
    GEOSPATIAL_UTIL updated with new functions included by John

--- ddl/packages/GEOSPATIAL_UTIL.sql
+++ ddl/packages/GEOSPATIAL_UTIL.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE package geospatial_util
+create or replace package geospatial_util
 as
    type    coordinate_type_tb       is table of varchar2(10);
    coordinate_type_const   constant coordinate_type_tb := coordinate_type_tb('LATITUDE','LONGITUDE');
-   
+
    type dms_coordinate_tp  is record 
    (
       degrees              number
@@ -10,7 +10,7 @@ as
    ,	seconds              number
    ,	cardinal             char(1)
    ); 
-   
+
    type dmm_coordinate_tp  is record 
    (
       degrees              number
@@ -41,6 +41,17 @@ as
     ,  p_coordinate_type   in varchar2 
    ) 
    return dms_coordinate_tp;
+-- 
+------------------------------------------------------- D D _ T O _ D M S _ S T R I N G 
+-- 
+    -- -- Converts DD (decimals, lat/long) coordinates to DMS (degrees, minutes, seconds, cardinal), returning colon-delimited string - D:M:S:C       
+   function dd_to_dms_string
+   (p_dd_coordinate     in number
+   ,p_coordinate_type   in varchar2
+   ,p_discard_seconds   in varchar2 default 'n'
+   ) 
+   return                  varchar2;   
+   
     -- -- Converts DD (decimals, lat/long) coordinates to DMM (degrees, decimal minutes, cardinal), returning 999 if parameters are not valid latitude or longitude 
    function dd_to_dmm
    (
@@ -56,8 +67,11 @@ end geospatial_util;
 -- PACKAGE BODY
 -- -----------------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE package body geospatial_util
+create or replace package body geospatial_util
 as
+--
+g_package constant varchar2(31) := $$plsql_unit || '.';
+---- 
 
    type    cardinal_tb              is table of char(1);
    cardinal_const          constant cardinal_tb := cardinal_tb('N','S','E','W');
@@ -111,7 +125,7 @@ as
                 v_coordinate := v_coordinate * -1;
             end if;
         end if;
-        
+
         return v_coordinate;
     end dmm_to_dd;
 
@@ -198,5 +212,81 @@ as
         return v_dmm_coordinate;
     end dd_to_dmm;
 
+-- 
+------------------------------------------------------- D D _ T O _ D M S _ S T R I N G 
+-- 
+   function dd_to_dms_string
+   (p_dd_coordinate     in number
+   ,p_coordinate_type   in varchar2
+   ,p_discard_seconds   in varchar2 default 'n'
+   ) 
+   return                  varchar2   
+   is
+   -- -- Converts DD (decimals, lat/long) coordinates to DMS (degrees, minutes, seconds, cardinal), returning colon-delimited string - D:M:S:C       
+      l_scope                    constant varchar2(61) := g_package||'dd_to_dms_string';
+      l_params                   logger.tab_param;
+      l_dms_coordinate           dms_coordinate_tp;
+      l_dd_coordinate            number := round(p_dd_coordinate,6);
+      l_d                        number default 0;
+      l_m                        number default 0;
+      l_s                        number default 0;
+      l_c                        char(1);
+   begin
+      --
+      logger.append_param (p_params => l_params, p_name => 'p_dd_coordinate', p_val => p_dd_coordinate);
+      logger.append_param (p_params => l_params, p_name => 'p_coordinate_type', p_val => p_coordinate_type);
+      logger.append_param (p_params => l_params, p_name => 'p_discard_seconds', p_val => p_discard_seconds);
+      logger.log_information (p_text    => 'Start'
+                             ,p_scope   => l_scope
+                             ,p_params  => l_params
+                             );      
+      --
+      if upper(p_coordinate_type) member of coordinate_type_const then
+         --
+         l_d := trunc(l_dd_coordinate);
+         l_m := trunc(abs(mod(l_dd_coordinate,1)) * 60);
+         --
+         if p_discard_seconds = 'n' then
+            l_s := round(mod(abs(mod(l_dd_coordinate,1)) * 60, 1) * 60, 0);
+         end if;
+         --
+         if upper(p_coordinate_type) = 'LATITUDE' then
+            if l_d < 0 then
+               l_d := l_d * -1;
+               l_c := 'S';
+            else
+               l_c := 'N';
+            end if;
+         else
+            if l_d < 0 then
+               l_d := l_d * -1;
+               l_c := 'W';
+            else
+               l_c := 'E';
+            end if;            
+         end if;
+      end if;
+      --
+      l_dms_coordinate.degrees  := l_d;
+      l_dms_coordinate.minutes  := l_m;
+      l_dms_coordinate.seconds  := l_s;
+      l_dms_coordinate.cardinal := l_c;
+      --
+      logger.log_information(p_text  => 'End'
+                            ,p_scope => l_scope
+                            );
+      --        
+      if p_discard_seconds = 'n' then
+         return l_d||':'||l_m||':'||l_s||':'||l_c;
+      else
+         return l_d||':'||l_m||':'||l_c;
+      end if;
+      --      
+   exception
+   when others then
+      logger.log_error('Unhandled exception', l_scope, null, l_params);
+      return ':';
+   end dd_to_dms_string;
+   --    
 end geospatial_util;
 /
