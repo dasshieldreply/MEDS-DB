fb8fbcc548fdef0ce980c54124a70055c9e11980
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-06-13 17:44:26 +0100

    Refactored to return all data types querying JOB_LOOKUPS

--- ddl/views/v_manual_form_navigation.sql
+++ ddl/views/v_manual_form_navigation.sql
@@ -1,19 +1,26 @@
 create or replace force editionable view V_MANUAL_FORM_NAVIGATION
-(
-   DATA_TYPE_USAGE
-,  DATA_TYPE_INDEX
-,  OBSERVATION_PAGE_ID
-,  TRACK_PAGE_ID
-) as
-   select 'BIOLUMINESCENCE', 	    7, 2101, null from dual union
-   select 'DEEP_SCATTERING', 		21, 2131, null from dual union
-   select 'MLO_BIRDS', 			    6, 2111, null from dual union
-   select 'MLO_CETACEANS', 		25, 2142, 2141 from dual union
-   select 'MLO_FISH', 				24, 2151, null from dual union
-   select 'MLO_JELLYFISH', 		20, 2161, null from dual union
-   select 'MLO_OTHERS', 			19, 2171, null from dual union
-   select 'MLO_PINNIPEDS', 		18, 2181, null from dual union
-   select 'MLO_REPTILES', 			17, 2191, null from dual union
-   select 'MLO_Seabed_Contacts',	93, 2121, null from dual union
-   select 'MLO_SHIP', 				23, 2201, null from dual 
+as
+select distinct
+   usage                                     as data_type_usage
+,  case usage
+      when 'BIOLUMINESCENCE' 		then 2101
+      when 'DEEP_SCATTERING' 		then 2131
+      when 'MLO_BIRDS'		 		then 2111
+      when 'MLO_CETACEANS' 	 	then 2142
+      when 'MLO_FISH'  		 		then 2151
+      when 'MLO_JELLYFISH' 	 	then 2161
+      when 'MLO_OTHERS' 	 		then 2171
+      when 'MLO_PINNIPEDS'   		then 2181
+      when 'MLO_REPTILES' 	 		then 2191
+      when 'MLO_Seabed_Contacts' then 2121
+      when 'MLO_SHIP'				then 2201
+      else	null
+   end                                       as observation_page_id
+,  case usage
+      when 'MLO_CETACEANS' 	 	then 2141
+      else	null
+   end                                       as track_page_id
+from job_lookups
+where type    = 'Data Type'
+and   generic = 1;
 ;
