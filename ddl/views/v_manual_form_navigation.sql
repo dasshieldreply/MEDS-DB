8236ebf22d7b66a2419a2b1b10c08fbc11d6e74c
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-06-13 16:51:45 +0100

    v_manual_form_navigation refactored to return observation and track pages

--- ddl/views/v_manual_form_navigation.sql
+++ ddl/views/v_manual_form_navigation.sql
@@ -2,17 +2,18 @@ create or replace force editionable view V_MANUAL_FORM_NAVIGATION
 (
    DATA_TYPE_USAGE
 ,  DATA_TYPE_INDEX
-,  PAGE_ID
+,  OBSERVATION_PAGE_ID
+,  TRACK_PAGE_ID
 ) as
-   select 'BIOLUMINESCENCE', 	    7, 2101 from dual union
-   select 'DEEP_SCATTERING', 		21, 2131 from dual union
-   select 'MLO_BIRDS', 			    6, 2111 from dual union
-   select 'MLO_CETACEANS', 		25, 2141 from dual union
-   select 'MLO_FISH', 				24, 2151 from dual union
-   select 'MLO_JELLYFISH', 		20, 2161 from dual union
-   select 'MLO_OTHERS', 			19, 2171 from dual union
-   select 'MLO_PINNIPEDS', 		18, 2181 from dual union
-   select 'MLO_REPTILES', 			17, 2191 from dual union
-   select 'MLO_Seabed_Contacts',	93, 2121 from dual union
-   select 'MLO_SHIP', 				23, 2201 from dual 
+   select 'BIOLUMINESCENCE', 	    7, 2101, null from dual union
+   select 'DEEP_SCATTERING', 		21, 2131, null from dual union
+   select 'MLO_BIRDS', 			    6, 2111, null from dual union
+   select 'MLO_CETACEANS', 		25, 2142, 2141 from dual union
+   select 'MLO_FISH', 				24, 2151, null from dual union
+   select 'MLO_JELLYFISH', 		20, 2161, null from dual union
+   select 'MLO_OTHERS', 			19, 2171, null from dual union
+   select 'MLO_PINNIPEDS', 		18, 2181, null from dual union
+   select 'MLO_REPTILES', 			17, 2191, null from dual union
+   select 'MLO_Seabed_Contacts',	93, 2121, null from dual union
+   select 'MLO_SHIP', 				23, 2201, null from dual 
 ;
