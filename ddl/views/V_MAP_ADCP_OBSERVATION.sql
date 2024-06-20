9777bfbdba968f565b3af463c91c837067e9c8bf
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-06-20 15:11:28 +0100

    V_MAP_ADCP_OBSERVATION and V_MAP_MLO_BIRDS_OBSERVATION finalized

--- ddl/views/V_MAP_ADCP_OBSERVATION.sql
+++ ddl/views/V_MAP_ADCP_OBSERVATION.sql
@@ -19,14 +19,46 @@ AS
    ,      b.temperature
    ,      b.speed_ship
    ,      b.dir_ship
-   from   param              p
-   ,      adcp_observation   a
-   ,      adcp_data          b
-   ,      job_tracking       c
+   ,      case e.column_name
+               when 'MEDS_JOB_NUMBER' then to_char(a.MEDS_JOB_NUMBER)
+               when 'MEDS_OBSERVATION_NUMBER' then to_char(a.MEDS_OBSERVATION_NUMBER)
+               when 'LATITUDE' then to_char(a.LATITUDE)
+               when 'LONGITUDE' then to_char(a.LONGITUDE)
+               when 'DATE_RECORDED' then to_char(a.date_recorded,'fmDD/MM/YYYY HH24:MI:SS')
+               when 'BIN_1_DISTANCE' then to_char(b.BIN_1_DISTANCE)
+               when 'BIN_SIZE' then to_char(b.BIN_SIZE)
+               when 'DEPTH_BSL_TRANS' then to_char(b.DEPTH_BSL_TRANS)
+               when 'DIR_SHIP' then to_char(b.DIR_SHIP)
+               when 'HEADING' then to_char(b.HEADING)
+               when 'JULIAN_DAY' then to_char(b.JULIAN_DAY)
+               when 'NO_BINS' then to_char(b.NO_BINS)
+               when 'NO_ENSEMBLES' then to_char(b.NO_ENSEMBLES)
+               when 'PROFILE' then to_char(b.PROFILE)
+               when 'SCALE_FACTOR' then to_char(b.SCALE_FACTOR)
+               when 'SPEED_SHIP' then to_char(b.SPEED_SHIP)
+               when 'SUPPLIER' then c.SUPPLIER               
+               when 'TEMPERATURE' then to_char(b.TEMPERATURE)
+               when 'U_BOTTOM' then to_char(b.U_BOTTOM)
+               when 'U_SHIP' then to_char(b.U_SHIP)
+               when 'V_BOTTOM' then to_char(b.V_BOTTOM)
+               when 'V_SHIP' then to_char(b.V_SHIP)
+               when 'WATER_DEPTH' then to_char(b.WATER_DEPTH)
+               when 'CRUISE_NAME' then p.CRUISE_NAME
+               else to_char(a.MEDS_JOB_NUMBER)
+          end as data_point_label
+   from   param                        p
+   ,      adcp_observation             a
+   ,      adcp_data                    b
+   ,      job_tracking                 c
+   ,      medsfilter_medslayer_label   d 
+   ,      medslayer_label              e
    where  a.meds_job_number         = p.meds_job_number   
    and    a.date_recorded between p.date_start and p.date_end
    and    sdo_anyinteract(a.location, p.location_rectangle) = 'TRUE'
    and    b.meds_job_number         = a.meds_job_number
    and    b.meds_observation_number = a.meds_observation_number
    and    c.meic_number             = p.meic_number
+   and    d.medsfilter         (+)  = p.medsfilter
+   and    d.medslayer          (+)  = p.medslayer
+   and    e.medslayer_label    (+)  = d.medslayer_label
 ;
--- ddl/views/V_MAP_MLO_BIRDS_OBSERVATION.sql
+++ ddl/views/V_MAP_MLO_BIRDS_OBSERVATION.sql
@@ -8,6 +8,7 @@ as
    )
    select p.icon
    ,      p.color
+   ,      p.cruise_name
    ,      a.meds_job_number
    ,      a.meds_observation_number
    ,      a.location
@@ -21,11 +22,32 @@ as
    ,      b.wd_spd
    ,      b.comments
    ,      b.file_reference
-   from   param                   p
-   ,      mlo_birds_observation   a
-   ,      mlo_birds_data          b
+   ,      case d.column_name
+               when 'MEDS_JOB_NUMBER' then to_char(a.MEDS_JOB_NUMBER)
+               when 'MEDS_OBSERVATION_NUMBER' then to_char(a.MEDS_OBSERVATION_NUMBER)
+               when 'LATITUDE' then to_char(a.LATITUDE)
+               when 'LONGITUDE' then to_char(a.LONGITUDE)
+               when 'DATE_RECORDED' then to_char(a.date_recorded,'dd Mon yyyy')
+               when 'COUNT' then to_char(b.COUNT)
+               when 'SHIP' then b.SHIP
+               when 'WD_DIR' then to_char(b.WD_DIR)
+               when 'WD_SPD' then to_char(b.WD_SPD)
+               when 'COMMENTS' then b.COMMENTS
+               when 'FILE_REFERENCE' then b.FILE_REFERENCE
+               when 'CRUISE_NAME' then p.CRUISE_NAME
+               else to_char(a.MEDS_JOB_NUMBER)
+          end as data_point_label
+   from   param                        p
+   ,      mlo_birds_observation        a
+   ,      mlo_birds_data               b
+   ,      medsfilter_medslayer_label   c 
+   ,      medslayer_label              d
    where  a.meds_job_number         = p.meds_job_number   
    and    a.date_recorded between p.date_start and p.date_end
    and    b.meds_job_number         = a.meds_job_number
    and    b.meds_observation_number = a.meds_observation_number
-   and    sdo_anyinteract(a.location, p.location_rectangle) = 'TRUE';
+   and    sdo_anyinteract(a.location, p.location_rectangle) = 'TRUE'
+   and    c.medsfilter      (+)     = p.medsfilter
+   and    c.medslayer       (+)     = p.medslayer
+   and    d.medslayer_label (+)     = c.medslayer_label
+   ;
