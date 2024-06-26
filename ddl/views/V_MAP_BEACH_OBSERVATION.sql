db8cb3550da6daa2fac85d7729b4475bf1224372
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-06-26 12:43:46 +0100

    MEDS_TestPlan version in the project Delivery folder added

--- ddl/views/V_MAP_BEACH_OBSERVATION.sql
+++ ddl/views/V_MAP_BEACH_OBSERVATION.sql
@@ -19,11 +19,31 @@ as
    ,      a.beach_category
    ,      a.beach_number
    ,      c.supplier
-   from   param               p
-   ,      beach_observation   a
-   ,      job_tracking        c
-   where  a.meds_job_number   = p.meds_job_number   
+   ,      p.cruise_name
+   ,      case e.column_name
+               when 'MEDS_JOB_NUMBER' then to_char(a.MEDS_JOB_NUMBER)
+               when 'MEDS_OBSERVATION_NUMBER' then to_char(a.MEDS_OBSERVATION_NUMBER)
+               when 'LATITUDE' then to_char(a.LATITUDE)
+               when 'LONGITUDE' then to_char(a.LONGITUDE)
+               when 'DATE_RECORDED' then to_char(a.date_of_survey,'fmDD/MM/YYYY HH24:MI:SS')
+               when 'COUNTRY' then a.COUNTRY
+               when 'BEACH_NAME' then a.beach_name
+               when 'BEACH_CATEGORY' then to_char(a.beach_category)
+               when 'BEACH_NUMBER' then a.beach_number
+               when 'SUPPLIER' then c.SUPPLIER               
+               when 'CRUISE_NAME' then p.CRUISE_NAME
+               else to_char(a.MEDS_JOB_NUMBER)
+          end as data_point_label
+   from   param                        p
+   ,      beach_observation            a
+   ,      job_tracking                 c
+   ,      medsfilter_medslayer_label   d 
+   ,      medslayer_label              e
+   where  a.meds_job_number     = p.meds_job_number   
    and    a.date_of_survey between p.date_start and p.date_end
    and    sdo_anyinteract(a.location, p.location_rectangle) = 'TRUE'
-   and    c.meic_number       = p.meic_number
+   and    c.meic_number         = p.meic_number
+   and    d.medsfilter      (+) = p.medsfilter
+   and    d.medslayer       (+) = p.medslayer
+   and    e.medslayer_label (+)= d.medslayer_label   
 ;
