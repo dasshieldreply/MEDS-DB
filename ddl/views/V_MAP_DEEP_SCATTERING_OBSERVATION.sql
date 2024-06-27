0163f72443dc2a9bbbe5a78bb7d70fbb656ac499
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-06-27 11:53:04 +0100

    V_MAP_DEEP_SCATTERING_OBSERVATION updated for label

--- ddl/views/V_MAP_DEEP_SCATTERING_OBSERVATION.sql
+++ ddl/views/V_MAP_DEEP_SCATTERING_OBSERVATION.sql
@@ -17,12 +17,28 @@ AS
    ,      b.ship
    ,      b.hi_cruise
    ,      b.remarks
+   ,      case d.column_name
+               when 'MEDS_JOB_NUMBER' then to_char(a.MEDS_JOB_NUMBER)
+               when 'MEDS_OBSERVATION_NUMBER' then to_char(a.MEDS_OBSERVATION_NUMBER)
+               when 'LATITUDE' then to_char(a.LATITUDE)
+               when 'LONGITUDE' then to_char(a.LONGITUDE)
+               when 'DATE_RECORDED' then to_char(a.date_recorded,'dd Mon yyyy hh24:mi:ss')
+               when 'SHIP' then b.ship
+               when 'HI_CRUISE' then b.hi_cruise
+               when 'REMARKS' then b.remarks
+               else to_char(a.MEDS_JOB_NUMBER)
+          end as data_point_label  
    from   param                        p
    ,      deep_scattering_observation  a
    ,      deep_scattering_data         b
+   ,      medsfilter_medslayer_label   c 
+   ,      medslayer_label              d   
    where  a.meds_job_number            = p.meds_job_number   
    and    a.date_recorded between p.date_start and p.date_end
    and    sdo_anyinteract(a.location, p.location_rectangle) = 'TRUE'
    and    b.meds_job_number            = a.meds_job_number
    and    b.meds_observation_number    = a.meds_observation_number
+   and    c.medsfilter             (+) = p.medsfilter
+   and    c.medslayer              (+) = p.medslayer
+   and    d.medslayer_label        (+) = c.medslayer_label
 ;
