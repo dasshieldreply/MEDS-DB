f284937c963dfc2e7d36aff65255e24c3bd12c85
dasshieldreply <d.arboittedossantos@shieldreply.com>
2024-06-27 11:22:07 +0100

    V_MAP_OMNI_AMBIENT_OBSERVATION updated for label

--- ddl/views/V_MAP_OMNI_AMBIENT_OBSERVATION.sql
+++ ddl/views/V_MAP_OMNI_AMBIENT_OBSERVATION.sql
@@ -1,5 +1,4 @@
-CREATE OR REPLACE FORCE EDITIONABLE VIEW "MEDSADMIN"."V_MAP_OMNI_AMBIENT_OBSERVATION"  
-AS 
+CREATE OR REPLACE FORCE EDITIONABLE VIEW "MEDSADMIN"."V_MAP_OMNI_AMBIENT_OBSERVATION" as
    with param as
    (
       select a.*
@@ -8,6 +7,7 @@ AS
    )
    select p.icon
    ,      p.color
+   ,      p.cruise_name
    ,      a.meds_job_number
    ,      a.meds_observation_number
    ,      a.location
@@ -18,12 +18,32 @@ AS
    ,		 b.depth
    ,		 b.platform_type
    ,		 b.comments
-   from   param                     p
-   ,      omni_ambient_observation  a
-   ,      omni_ambient_data         b
+   ,      b.country
+   ,      case d.column_name
+               when 'MEDS_JOB_NUMBER' then to_char(a.MEDS_JOB_NUMBER)
+               when 'MEDS_OBSERVATION_NUMBER' then to_char(a.MEDS_OBSERVATION_NUMBER)
+               when 'LATITUDE' then to_char(a.LATITUDE)
+               when 'LONGITUDE' then to_char(a.LONGITUDE)
+               when 'DATE_RECORDED' then to_char(a.date_recorded,'dd Mon yyyy hh24:mi:ss')
+               when 'SHIP' then b.ship
+               when 'CRUISE_NAME' then p.cruise_name
+               when 'DEPTH' then to_char(b.depth)
+               when 'PLATFORM_TYPE' then b.platform_type
+               when 'COMMENTS' then b.comments
+               when 'COUNTRY' then b.country
+               else to_char(a.MEDS_JOB_NUMBER)
+          end as data_point_label   
+   from   param                      p
+   ,      omni_ambient_observation   a
+   ,      omni_ambient_data          b
+   ,      medsfilter_medslayer_label c 
+   ,      medslayer_label            d   
    where  a.meds_job_number         = p.meds_job_number   
    and    a.date_recorded between p.date_start and p.date_end
    and    sdo_anyinteract(a.location, p.location_rectangle) = 'TRUE'   
    and    b.meds_job_number         = a.meds_job_number
    and    b.meds_observation_number = a.meds_observation_number
+   and    c.medsfilter          (+) = p.medsfilter
+   and    c.medslayer           (+) = p.medslayer
+   and    d.medslayer_label     (+) = c.medslayer_label
 ;
